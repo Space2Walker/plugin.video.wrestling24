e129a9b86d0b1aeedfeac8d5de9714e6bab61254
Space2Walker <amuenn@googlemail.com>
2019-03-05 21:47:57 +0100

    Update wrestling24.py
    new functions for wrestling24
    old functions removed

--- resources/lib/wrestling24.py
+++ resources/lib/wrestling24.py
@@ -9,141 +9,70 @@ import resources.lib.helper as helper
 
 
 
-
-
-
-
-
-# funstion that takes the wrestling24 nav menu item number and graps the shows 
-def get_shows(x):
+def get_shows(cat_id):
     url = 'http://watchwrestling24.net/'
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, "lxml")
-    crap = soup.find_all(id="nav-menu-item-" + str(x))
-    shows = crap[0].ul.find_all("li")
+    shows = []
+    
+    soup = helper.get_soup(url)
+
+    div = soup.find_all(id="nav-menu-item-" + str(cat_id))
+    show_div = div[0].ul.find_all("a")
+
+    for li in show_div: 
+        shows.append(
+            dict([
+                ('show', li.text),
+                ('link', li.get('href')),
+
+            ]))
+
     return shows
 
-# funstion that takes the wrestling24 category url and graps the episodes 
-def get_episodes(x):
-    res = requests.get(x)
-    soup = BeautifulSoup(res.text, "lxml")
-    episodes = soup.find_all("h3", attrs={"class": "h4"})
-    return episodes
+def get_episodes(url):
+    soup = helper.get_soup(url)
+    episode_info = []
+    episodes = soup.find_all("div", class_="picture-content")
+    
+    for info in episodes:
+        res = ''
+        views = ''
+        uploader = ''
+        title = info.a.get('title')
+        img = info.a.img.get('src')
 
-# funstion that takes the wrestling24 episode url and graps the real links at education-load
-def get_links(x):
-    res = requests.get(x)
-    soup = BeautifulSoup(res.text, "lxml")
-    crap = soup.find_all("p", attrs={"class": "plinks"})
-    int_links = []
+        episode_info.append(
+            dict([
+                ('title', title),
+                ('link', info.a.get('href')),
+                ('duration', 0),
+                ('thumb', img),
+                ('res', res),
+                ('views', views),
+                ('uploader', uploader),
+                #('category', category)
+                ]))
+    return episode_info
+
+def get_links(url):
+    soup = helper.get_soup(url)
     links = []
-    for i in crap:
-        content = i.next_sibling.a['href']
-        int_links.append(content)
-    for i in int_links:
-        res1 = requests.get(i, headers={'referer': "http://watchwrestling24.net"})
-        soup1 = BeautifulSoup(res1.text, "lxml")
-        crap1 = soup1.find("iframe", attrs={"class": "embed-responsive-item"})
-        links.append(crap1.attrs['src'])
+
+    div = soup.find_all("p", attrs={"class": "plinks"})
+
+    for p in div:
+        hoster = p.text
+        content = p.next_sibling.find_all('a')
+
+        hoster_dict = {}
+        hoster_dict['hoster'] = hoster
+        hoster_dict['link'] = []
+
+        for link in content:
+            hoster_dict['link'].append(link.get('href'))
+   
+        links.append(hoster_dict)
+
     return links
-
-## contains al list of all availabye wwe shows
-wwe=get_shows(16086)
-## contains al list of the last episodes of cattegory url
-nxt=get_episodes(wwe[0].a['href'])
-## contains al list of all links for a episode
-link=get_links(nxt[0].a['href'])
-
-
-
-
-
-
-
-#################################
-#			get_cats			#
-#################################
-'''
-crawls the Catergorys from xvideos.com
-and returns them as a list of dicts
-
-[{'category': 'Pornos auf Deutsch', 'link': 'https://xvideos.com/lang/deutsch'}, 
- {'category': '3d', 'link': 'https://xvideos.com/?k=3d&top'}]
-'''
-def get_cats():
-	url = 'https://chaturbate.com'
-	cats = []
-	soup = helper.get_soup(url)
-	ul = soup.find("ul", class_="sub-nav")
-
-	for li in ul.find_all("li"): 
-		cats.append(
-			dict([
-				('category', li.text),
-				('link', url + li.a.get('href'))
-			]))
-
-	return cats
-
-#################################
-#			get_vids			#
-#################################
-'''
-crawls a given url form chaturbate.com for videos
-and returns them as a list of dicts
-if a catergory is given it will be added to the dict
-
-a returnd dict looks like this
-	 KEYS	 VALUE 
-[{ 'title': 'BF HAVE 8 INC BUT YOUR ', 
-    'link': 'https://chaturbate.com/nasty_girl_masturbate',
-'duration': '5 min', 
-   'thumb': 'https://img-hw.com/videos/thumbs169/a3/ed/36/a3ed367bcb5a69a9ad.14.jpg', 
-     'res': '720p', 
-   'views': '13k',
-'uploader': 'hans',
-'category': 'Grany'}]
-'''
-def get_vids(url, category='none'):
-
-	hardcoded = 'https://chaturbate.com'
-	video_info = []
-	videos = [] 
-
-	soup = helper.get_soup(url)
-
-	videos = soup.find_all("li", class_="room_list_room")
-
-	for info in videos:
-		res = ''
-		title = info.find("a", href=True).get('href')[1:-1]
-		uploader = info.find("a", href=True).get('href')
-		img = info.find("a", href=True).find('img').get('src')
-
-		# views and time are only seperatot bei "," on the site
-		duraview = info.find("li", class_="cams").text.split(",")
-		views = duraview[1]
-
-		#if duraview[0].find("h") != -1:  #
-		#	h = float(duraview[0][:-4])
-		#	duration = (h * 60) * 60
-		
-		#else: 
-		#	duration = duraview[0][:-5] * 60
-
-		video_info.append(
-			dict([
-				('title', title),
-				('link',  hardcoded + uploader),
-				('duration', 0),
-				('thumb', img),
-				('res', res),
-				('views', views),
-				('uploader', title),
-				('category', category)
-				]))
-	return video_info
-
 
 def play_video(_handle, video):
     """
